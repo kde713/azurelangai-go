6c9791d605d162a23703100e29cc0cba116a665b
Dongeon Kim <kde713@gmail.com>
2023-07-10 18:58:36 +0900

    Implement LROResult Unmarshal logic

--- textanalysis/v20230401/lromodel.go
+++ textanalysis/v20230401/lromodel.go
@@ -1,5 +1,9 @@
 package v20230401
 
+import (
+	"encoding/json"
+)
+
 type JobStatus string
 
 const (
@@ -12,17 +16,87 @@ const (
 	StatusSucceeded          JobStatus = "succeeded"
 )
 
-type LROResult struct {
-	// Kind Enumeration of supported Text Analysis long-running operation task results.
-	Kind               TaskKind    `json:"kind"`
-	LastUpdateDateTime string      `json:"lastUpdateDateTime"`
-	Results            interface{} `json:"-"`
-	Status             string      `json:"status"`
-	TaskName           string      `json:"taskName"`
+type LROKind string
+
+const (
+	LROKindEntityRecognition        LROKind = "EntityRecognitionLROResults"
+	LROKindKeyPhraseExtraction      LROKind = "KeyPhraseExtractionLROResults"
+	LROKindSentimentAnalysis        LROKind = "SentimentAnalysisLROResults"
+	LROKindExtractiveSummarization  LROKind = "ExtractiveSummarizationLROResults"
+	LROKindAbstractiveSummarization LROKind = "AbstractiveSummarizationLROResults"
+)
+
+type commonLROResult struct {
+	Kind               LROKind   `json:"kind"`
+	LastUpdateDateTime string    `json:"lastUpdateDateTime"`
+	Status             JobStatus `json:"status"`
+	TaskName           string    `json:"taskName"`
 }
 
 type resultsOnly[T any] struct {
 	Results T `json:"results"`
+}
+
+var _ json.Unmarshaler = (*LROResult)(nil)
+
+type LROResult struct {
+	// Kind Enumeration of supported Text Analysis long-running operation task results.
+	Kind               LROKind
+	LastUpdateDateTime string
+	Results            interface{}
+	Status             JobStatus
+	TaskName           string
+}
+
+func (r *LROResult) UnmarshalJSON(bytes []byte) error {
+	// Map common keys
+	var common commonLROResult
+	if err := json.Unmarshal(bytes, &common); err != nil {
+		return err
+	}
+	r.Kind = common.Kind
+	r.LastUpdateDateTime = common.LastUpdateDateTime
+	r.Status = common.Status
+	r.TaskName = common.TaskName
+
+	if r.Status != StatusSucceeded {
+		// If the task is not complete, return immediately
+		return nil
+	}
+
+	switch r.Kind {
+	case LROKindEntityRecognition:
+		var results resultsOnly[EntitiesResult]
+		if err := json.Unmarshal(bytes, &results); err != nil {
+			return err
+		}
+		r.Results = results.Results
+	case LROKindKeyPhraseExtraction:
+		var results resultsOnly[KeyPhraseResult]
+		if err := json.Unmarshal(bytes, &results); err != nil {
+			return err
+		}
+		r.Results = results.Results
+	case LROKindSentimentAnalysis:
+		var results resultsOnly[SentimentResponse]
+		if err := json.Unmarshal(bytes, &results); err != nil {
+			return err
+		}
+		r.Results = results.Results
+	case LROKindExtractiveSummarization:
+		var results resultsOnly[ExtractiveSummarizationResult]
+		if err := json.Unmarshal(bytes, &results); err != nil {
+			return err
+		}
+		r.Results = results.Results
+	case LROKindAbstractiveSummarization:
+		var results resultsOnly[AbstractiveSummarizationResult]
+		if err := json.Unmarshal(bytes, &results); err != nil {
+			return err
+		}
+		r.Results = results.Results
+	}
+	return nil
 }
 
 type Tasks struct {
